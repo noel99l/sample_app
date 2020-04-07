31de779c381fce16fa440bd77ab51b857fa0bff8
noel99l <noel1991@hotmail.co.jp>
2020-04-07 07:39:36 +0000

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,11 +4,23 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
+  # ユーザーのセッションを永続的にする
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
   # 現在ログイン中のユーザーを返す（いる場合）
   def current_user
-    if session[:user_id]
-      @current_user ||=User.find_by(id: session[:user_id])
-      # @current_user = @current_user || User.find_by(id: session[:user_id]) の短縮形
+    if (user_id = session[:user_id]) #(ユーザーIDにユーザーIDのセッションを代入した結果) ユーザーIDのセッションが存在すれば
+      @current_user ||= User.find_by(id: user_id) # @current_user = @current_user || User.find_by(id: session[:user_id]) の短縮形
+    elsif (user_id = cookies.signed[:user_id]) #elsif cookies.signed[:user_id] と同じ、この時点でローカル変数user_idに代入することでcookiesメソッドの使用を１回のみに留めている。
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
   
@@ -17,8 +29,15 @@ module SessionsHelper
     !current_user.nil?
   end
   
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
-  end    
+  end
 end
